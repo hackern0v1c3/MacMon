9c94a9154fd8c4d4fc1e01f6250be0fbe199692e
hackernovice <kev@hackernovice.com>
2017-10-27 16:16:33 -0400

    Worked on learning mode.  Updated readme with some credits

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ This script assumes that it will run on an Ubuntu Linux system running Python3. 
 This script does not do anything with IPv6 Sorry!
 
 This script saves your emai username and password in clear text into a config file.  These credentials are required for the ability to send emails.  The config file should be restricted to ONLY the user who will run the script.  Use this script at your own risk!
+
+Right now this script requires nmap. I plan on changing that at some point but no promises.
+
+Credit:
+  Script idea- Brian at 7ms.us for coming up with the idea.
+  https://www.peterbe.com/plog/uniqifiers-benchmark I stole the function for unique arrays from here
--- macmon.py
+++ macmon.py
@@ -10,8 +10,6 @@ import configparser
 
 #Setup configuration file structure
 config = configparser.ConfigParser()
-config['NETWORKING'] = {}
-config['EMAIL'] = {}
 
 #Define arguments
 parser = argparse.ArgumentParser()
@@ -22,11 +20,22 @@ group.add_argument("-c", "--check", action="store_true", help="Scan the network 
 parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output on the command prompt for troubleshooting")
 args = parser.parse_args()
 
+#This function takes in an array, removes it's duplicates, and returns a unique array.  Found here https://www.peterbe.com/plog/uniqifiers-benchmark
+def unique_array(seq):
+   # Not order preserving
+   keys = {}
+   for e in seq:
+       keys[e] = 1
+   return keys.keys()
+
 #This function takes in a string which should be a comma seperated list of subnets.
 #It splits the list and verifies that each subnet is valid.
 def verifySubnets(subnets):
   split_subnets = subnets.split(',')
   for network in split_subnets:
+    if '/' not in network:
+      print("Invalid subnet %s.  Must include mask bits.  For example %s/24" % (network, network))
+      return False
     try:
       ipaddress.ip_network(network)
     except:
@@ -37,6 +46,9 @@ def verifySubnets(subnets):
 #The main function to call if the --setup option is selected.
 #This function should ask the user a series of questions, verify the answers, and create a configuration file.
 if args.setup:
+  config['NETWORKING'] = {}
+  config['EMAIL'] = {}
+
   os.system('clear')
   input("This setup wizard will ask you a series of questions which will be used to build a configuration file.  Press enter to continue.\n")
   os.system('clear')
@@ -92,13 +104,17 @@ if args.setup:
 
   try:
     with open('macmon.cfg', 'w') as configfile:
+      if args.verbose:
+        print("Saving config file to macmon.cfg\n")
       config.write(configfile)
   except:
     os.system('clear')
-    print("error saving macmon.cfg")
+    print("error saving macmon.cfg\n")
     sys.exit(1)
 
   try:
+    if args.verbose:
+      print("chmod macmon.cfg 600\n")
     os.chmod("macmon.cfg", 0o600)
   except:
     os.system('clear')
@@ -112,7 +128,45 @@ if args.setup:
 #This function should check for a configuration file and verify the subnets
 #Then this function should scan each subnet and record all identified MAC addresses
 elif args.learn:
+  try:
+    config.read("macmon.cfg")
+  except:
+    print("Unable to read macmon.cfg.  Exiting")
+    sys.exit(1)
+
+  if not verifySubnets(config['NETWORKING']['Subnets']):
+    print("Invalid information in macmon.cfg.  Ending script")
+    sys.exit(1)
+
+  subnets = config['NETWORKING']['Subnets'].split(',')
+  for subnet in subnets:
+    os.system('nmap -n -sn %(subnet)s' % locals())
+
+  arptable = ""
+  try:
+    f = open("/proc/net/arp", "r")
+    arptable = f.read()
+    f.close()
+  except:
+    print("Error: Unable to read arp table /proc/net/arp")
+    sys.exit(1)
+
+  mac_address_array = []
+  for line in arptable.splitlines():
+    mac_address_array.append(line.split()[3])
+
+  mac_address_array.pop(0)
+
+  ######## HERE I SHOULD TRY AND READ MAC ADDRESS FILE
+  ######## IF IT EXISTS SORT UNIQUE
+  ######## THEN SEARCh NEW SCAN RESULTS FOR ADDITIONAL ADDRESSES
+  ######## ADD ANY NEW ADDRESSES TO OLD LIST
+  ######## WRITE BACK TO FILE
+
+  ######## IF FILES DIDN'T EXIST JUST WRITE A NEW ONE WITH NEW SORTED UNIQUE ARRAY
+
   print("Learning Complete")
+  print(sorted(unique_array(mac_address_array)))
   sys.exit(0)
 
 #The main function to call if the --check option is selected.
