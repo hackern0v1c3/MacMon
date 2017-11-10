d43fec0053897442abef4f00c4cd92f7798d47b9
hackernovice <kev@hackernovice.com>
2017-11-10 13:31:04 -0500

    Setup logger for express

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+"use strict";
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const morgan = require('morgan');
+const logger = require('./private/logger.js');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const config = require('./private/config.js');
+
+const index = require('./routes/index');
+const users = require('./routes/users');
 
 var app = express();
 
@@ -16,7 +20,7 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(morgan('default', { 'stream': logger.stream}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -34,10 +38,10 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // set locals, only providing error in developmentlogger
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  res.locals.error = config.environment === 'development' ? err : {};
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
