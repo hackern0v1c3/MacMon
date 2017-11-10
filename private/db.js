4a8a03f1296ede90bb07cac393e9e44c37c935de
hackernovice <kev@hackernovice.com>
2017-11-10 15:56:49 -0500

    Added database operations for user accounts to db.js

--- private/db.js
+++ private/db.js
@@ -70,5 +70,103 @@ module.exports.assets = {
 			});
 		});
 	}
+}
 
+//Export SQL Queries For Users
+module.exports.users = {
+	insertNew: function(userName, userPassword, userRole, userEmail, cb) {
+		if(userName === null || userPassword === null || userRole === null || userEmail === null) {
+			return cb('username, password, role, and email cannot be null');
+		}
+		
+		bcrypt.genSalt(config.hashStrength, function(err, salt) {
+			if(err){return cb(err);}
+			bcrypt.hash(userPassword, salt, null, function(err, hash) {
+				if(err){return cb(err);}
+				pool.getConnection(function(err, connection) {
+					if(err){return cb(err);}
+					connection.query('INSERT INTO users (userName, userPass, userRole) VALUES (?, ?, ?, ?)', [userName, hash, userRole], function (err, results, fields) {
+						connection.release();
+						if(err){return cb(err);}
+						return cb(null);
+					}); 
+				});
+			});
+		});
+	},
+
+	//Inserts a new record into the users table
+	insertNew: function(userName, userPassword, userRole, cb) {
+		if(userName === null || userPassword === null || userRole === null) {
+			return cb('username, password, and role cannot be null');
+		}
+		
+		bcrypt.genSalt(config.hashStrength, function(err, salt) {
+			if(err){return cb(err);}
+			
+			bcrypt.hash(userPassword, salt, null, function(err, hash) {
+				if(err){return cb(err);}
+		
+				pool.getConnection(function(err, connection) {
+					if(err){return cb(err);}
+					connection.query('INSERT INTO users (userName, userPass, userRole) VALUES (?, ?, ?)', [userName, hash, userRole], function (err, results, fields) {
+						connection.release();
+						if(err){return cb(err);}
+						return cb(null);
+					}); 
+				});
+			});
+		});
+	},
+	
+	//Searches database for a username and password pair.  If a match is found the entire user object is returned.  Used for authentication
+	selectUsernameAndPassword: function(userName, userPassword, cb) {
+		if(userName === null || userPassword === null){
+			return cb('username, and password cannot be null');
+		}
+	
+		pool.getConnection(function(err, connection) {
+			if(err){return cb(err, null);}
+	
+			connection.query('SELECT id, userName, roleName, userPass from usersWithRoles WHERE userName = ?', [userName], function (err, results, fields) {
+				connection.release();
+				if(err){return cb(err, null);}
+	
+				if (results.length === 1){
+					bcrypt.compare(userPassword, results[0].userPass, function(err, res) {
+						if (res == true){
+							results[0].userPass = null;
+							cb(null, results[0]);
+						} else {
+							cb(null, null);
+						}
+					});
+				} else {
+					cb(null, null);
+				}
+			});
+		});
+	},
+	
+	//Searches database for a user by their id.  Used for already logged in sessions
+	selectById: function(id, cb) {
+		if (id === null) {
+			return cb('id cannot be null');
+		}
+	
+		pool.getConnection(function(err, connection) {
+			if(err){return cb(err, null);}
+		
+			connection.query('SELECT id, userName, roleName from usersWithRoles WHERE id = ?', [id], function (err, results, fields) {
+				connection.release();
+				if(err){return cb(err, null);}
+				
+				if (results.length === 1){
+					cb(null, results[0]);
+				} else {
+					return cb('id not found', null);
+				}
+			});
+		});
+	}
 }
