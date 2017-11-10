770a6335815665910ed679aa6c5a25fe76d64b15
hackernovice <kev@hackernovice.com>
2017-11-10 13:35:01 -0500

    removed depricated morgan option

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(morgan('default', { 'stream': logger.stream}));
+app.use(morgan('combined', { 'stream': logger.stream}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
