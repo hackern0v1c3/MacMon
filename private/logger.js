83739433fffae7eb843b61e2527ad6bd8214b494
hackernovice <kev@hackernovice.com>
2017-11-10 11:03:46 -0500

    Setup basic logger module for console logging

---
+++ private/logger.js
@@ -0,0 +1,26 @@
+//Read config file
+const config = require('../private/config.js');
+
+//Sets up a reusable winston logger for the rest of the application.  It reads the log level from the "environment" value in config.
+const winston = require("winston");
+
+var logLevel = 'error';
+
+if (config.environment === 'development'){
+  var logLevel = 'debug';
+}
+
+const level = logLevel;
+
+const logger = new winston.Logger({
+    transports: [
+        new winston.transports.Console({
+            level: level,
+            timestamp: function () {
+                return (new Date()).toISOString();
+            }
+        })
+    ]
+});
+
+module.exports = logger
