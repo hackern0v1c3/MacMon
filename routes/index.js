4931cbba872057083585f55f76bf3180c5be072b
hackernovice <kev@hackernovice.com>
2017-12-26 16:22:55 -0500

    Added asset blocking to approved asset page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../controllers/db.js');
+var blocker = require('../controllers/run_blocker.js');
 
 /* GET home page. */
 //Redirect if not logged in
@@ -10,7 +11,19 @@ router.get('/', function (req, res) {
   } else {
     db.assets.returnApprovedAssets(function(err, results, fields){
       if(!err){
-        res.render('index', { username: req.user.userName, assets: results })
+        var blockedDevices = blocker.getBlockedDevices();
+
+        var resultsIncludingBlocks = results.map(function cb(eachResult) {
+          var indexOfDevice = blockedDevices.findIndex(x => x.name == eachResult.IP);
+          if (indexOfDevice > -1) {
+            eachResult.blocked = true;
+          } else {
+            eachResult.blocked = false;
+          }
+          return eachResult;
+        });
+
+        res.render('index', { username: req.user.userName, assets: resultsIncludingBlocks })
       } else {
         res.status(500).send('Internal server error: Error retrieving data');
       }
