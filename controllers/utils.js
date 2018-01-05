6a8ae81750938ebfb1b0b687cb6d7ef1be71d61c
hackernovice <kev@hackernovice.com>
2018-01-05 11:31:48 -0500

    Created a utility controller for things like validating ip addresses and mac addresses

---
+++ controllers/utils.js
@@ -0,0 +1,16 @@
+"use strict";
+
+exports.validateIPaddress = function(ipaddress) {
+  if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress))  
+  {  
+    return (true);
+  }   
+  return (false);
+}
+
+exports.validateMac = function(macAddress) {
+  if (/^([0-9a-fA-F][0-9a-fA-F]:){5}([0-9a-fA-F][0-9a-fA-F])$/.test(macAddress)) {
+    return (true);
+  }
+  return (false);
+}
