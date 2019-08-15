b4c9ca1628973e0b433d8a4a7d0bfa4641c60d1b
hackern0v1c3 <kev@hackernovice.com>
2019-08-15 14:43:45 -0400

    changed bcrypt library again

--- appcode/controllers/db.js
+++ appcode/controllers/db.js
@@ -2,7 +2,7 @@
 
 //Import modules
 const mysql = require('mysql');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 //Setup SQL Server connection
 var pool = mysql.createPool({
