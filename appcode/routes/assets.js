418b96b0f31b954627e747f902f10fcf494a2580
hackern0v1c3 <kev@hackernovice.com>
2019-08-20 14:17:59 -0400

    added validators to assets routes

--- appcode/routes/assets.js
+++ appcode/routes/assets.js
@@ -6,10 +6,19 @@ const logger = require('../controllers/logger.js');
 const utils = require('../controllers/utils.js');
 const run_nmap = require('../controllers/run_nmap.js');
 var blocker = require('../controllers/run_blocker.js');
+const validateIp = require('../validators/ip_address.js');
+const validateMac = require('../validators/mac_address.js');
+const validateAsset = require('../validators/asset.js');
 
 /* Routes for Asset Operations */
 //For retrieving an asset by MAC address
 router.get('/:MAC', user.can('read data'), function (req, res) {
+  const validationError = validateMac(req.params.MAC);
+  if ( validationError != null){
+    logger.debug(`MAC Address validation failed: ${validationError}`);
+    return res.status(422).send('Improperly Formed MAC Address');
+  }
+
   db.assets.returnAsset(req.params.MAC, function(err, asset){
     if(!err){
       res.status(200).send(asset);
@@ -22,6 +31,12 @@ router.get('/:MAC', user.can('read data'), function (req, res) {
 
 //For approving a MAC address
 router.get('/approve/:MAC', user.can('update data'), function (req, res) {
+  const validationError = validateMac(req.params.MAC);
+  if ( validationError != null){
+    logger.debug(`MAC Address validation failed: ${validationError}`);
+    return res.status(422).send('Improperly Formed MAC Address');
+  }
+
   db.assets.approveAsset(req.params.MAC, function(err){
     if(!err){
       res.status(200).send('MAC Approved');
@@ -34,6 +49,12 @@ router.get('/approve/:MAC', user.can('update data'), function (req, res) {
 
 //For deleting an asset by MAC address
 router.get('/delete/:MAC', user.can('delete data'), function (req, res) {
+  const validationError = validateMac(req.params.MAC);
+  if ( validationError != null){
+    logger.debug(`MAC Address validation failed: ${validationError}`);
+    return res.status(422).send('Improperly Formed MAC Address');
+  }
+
   db.assets.deleteAsset(req.params.MAC, function(err){
     if(!err){
       res.status(200).send('MAC Deleted');
@@ -46,6 +67,11 @@ router.get('/delete/:MAC', user.can('delete data'), function (req, res) {
 
 //For toggling blocking a device on the network by IP Address
 router.get('/block/:IpAddress', user.can('write data'), function (req, res) {
+  const validationError = validateIp(req.params.IpAddress);
+  if ( validationError != null){
+    logger.debug(`IP Address validation failed: ${validationError}`);
+    return res.status(422).send('Improperly Formed IP Address');
+  }
 
   //Validate that the IP address is legitimate
   if (utils.validateIPaddress(req.params.IpAddress)) {
@@ -59,6 +85,13 @@ router.get('/block/:IpAddress', user.can('write data'), function (req, res) {
 
 /* POST for updating data */
 router.post('/update', user.can('update data'), function(req, res) {
+  const validationError = validateAsset(req.body);
+  if ( validationError != null){
+    logger.debug(`Received asset object: ${req.body}`);
+    logger.debug(`Asset validation failed: ${validationError}`);
+    return res.status(422).send('Improperly Formed Asset Data');
+  }
+  
   db.assets.updateAsset(req.body, function(err){
     if(!err){
       res.status(200).send('MAC Updated');
@@ -71,6 +104,18 @@ router.post('/update', user.can('update data'), function(req, res) {
 
 //Post for starting a nmap scan
 router.post('/scan', user.can('update data'), function(req, res) {
+  const validationErrorMac = validateMac(req.body.MAC);
+  if ( validationErrorMac != null){
+    logger.debug(`MAC Address validation failed: ${validationErrorMac}`);
+    return res.status(422).send('Improperly Formed MAC Address');
+  }
+
+  const validationErrorIp = validateIp(req.body.IP);
+  if ( validationErrorIp != null){
+    logger.debug(`IP Address validation failed: ${validationErrorIp}`);
+    return res.status(422).send('Improperly Formed IP Address');
+  }
+  
   run_nmap.runOnce(req.body.MAC, req.body.IP);
   res.status(200).send();
 });
