d231512b1324f41810998ad2e2c2afafddda3aa4
hackern0v1c3 <kev@hackernovice.com>
2019-08-21 12:47:48 -0400

    added input validation

--- appcode/routes/mailer.js
+++ appcode/routes/mailer.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const user = require('../controllers/roles.js');
 const logger = require('../controllers/logger.js');
 const mailer = require('../controllers/mailer.js');
+const validateEmailTest = require('../validators/email_test.js');
 
 /* POST request to send test email */
 //Redirect if not logged in
@@ -10,10 +11,14 @@ router.post('/', function (req, res) {
   if (!req.user) {
     res.status(401).send('Please log in to send test emails');
   } else {
-    var emailSettingsToTest = req.body;
-
+    const validationError = validateEmailTest(req.body);
+    if ( validationError != null){
+      logger.debug(`Email test settings validation failed: ${validationError}`);
+      return res.status(422).send('Improperly Formed Email Test Settings');
+    }
+    
     logger.info("Attempting to send test email");
-    mailer.testSendMessage(emailSettingsToTest, function(err, result){
+    mailer.testSendMessage(req.body, function(err, result){
       if (!err){
         logger.info("Test email sent succesfully");
         res.status(200).send("Test email sent succesfully.");
