fe8a33cee2bc2d63a19afc1aa2c0d54e07ed172f
hackern0v1c3 <kev@hackernovice.com>
2019-08-26 10:59:21 -0400

    added validator to password reset route

--- appcode/routes/passwordreset.js
+++ appcode/routes/passwordreset.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const db = require('../controllers/db.js');
 const user = require('../controllers/roles.js');
 const logger = require('../controllers/logger.js');
+const validatePasswordReset = require('../validators/password_reset.js');
 
 /* POST for restting user passwords */
 router.post('/', function(req, res) {
   if (!req.user) {
     res.redirect('/login');
   } else {
+    const validationError = validatePasswordReset(req.body.password, req.body.confirmPassword);
+    if ( validationError != null){
+      logger.debug(`Password validation failed: ${validationError}`);
+      return res.status(422).send('Invalid password reset request');
+    }
+
     db.users.updatePassword(req.user.userName, req.body.password, function(err) {
       if(!err) {
         logger.debug(`Password reset for ${req.user.userName}`);
